73ab9034ab47c8b4e8ba82fc75d88c50c865ff94
ConCaLon <chuanan183@gmail.com>
2026-09-02 13:55:16 +0700

    feat(auth-service): add userId claim to JWT

--- auth-service/src/main/java/vn/edu/crs/authservice/service/AuthService.java
+++ auth-service/src/main/java/vn/edu/crs/authservice/service/AuthService.java
@@ -36,15 +36,7 @@ public class AuthService {
             );
         }
 
-        String token = jwtUtil.generateToken(
-                user.getUsername(),
-                user.getRole()
-        );
-
-        return new LoginResponseDTO(
-                token,
-                user.getUsername(),
-                user.getRole()
-        );
+        String token = jwtUtil.generateToken(user.getId(), user.getUsername(), user.getRole());
+        return new LoginResponseDTO(user.getId(), token, user.getUsername(), user.getRole());
     }
 }
